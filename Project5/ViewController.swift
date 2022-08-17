0c56a73593dc8f8084515b6b0bf0d71c7cc0ce42
hassandaru <hassandaru@gmail.com>
2022-08-17 22:32:58 +1000

    added error messages via UIAlertAction

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -50,7 +50,7 @@ class ViewController: UITableViewController {
 
         let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
             guard let answer = ac?.textFields?[0].text else { return }
-            self?.submit(answer)
+            self?.submit(answer: answer)
         }
 
         ac.addAction(submitAction)
@@ -82,8 +82,11 @@ class ViewController: UITableViewController {
             return misspelledRange.location == NSNotFound
     }
     
-    func submit(_ answer: String) {
+    func submit(answer: String) {
         let lowerAnswer = answer.lowercased()
+
+        let errorTitle: String
+        let errorMessage: String
 
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
@@ -92,9 +95,25 @@ class ViewController: UITableViewController {
 
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+
+                    return
+                } else {
+                    errorTitle = "Word not recognised"
+                    errorMessage = "You can't just make them up, you know!"
                 }
+            } else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
             }
+        } else {
+            guard let title = title?.lowercased() else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title)"
         }
+
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 }
 
